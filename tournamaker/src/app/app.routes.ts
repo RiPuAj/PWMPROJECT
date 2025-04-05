92dd425fa76cf99ecdb42077361f121e8701b40d
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-04-05 17:52:29 +0100

    Botones al register linkeados

--- tournamaker/src/app/app.routes.ts
+++ tournamaker/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/forms/login/login.component';
 import {LandingComponent} from './pages/landing/landing.component';
+import {RegisterFormComponent} from './pages/forms/register-form/register-form.component';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "", component: LandingComponent }
+  { path: "", component: LandingComponent },
+  { path: "register", component: RegisterFormComponent }
 ];
