114db92aaaa2db3b7730d57075b391c4b94c19ed
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-04-05 13:04:53 +0100

    "esqueleto" de la app con Angular montado

--- tournamaker/src/app/app.routes.ts
+++ tournamaker/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/forms/login/login.component';
+import {LandingComponent} from './pages/landing/landing.component';
 
-export const routes: Routes = [{
-  path: "login", component: LoginComponent,
-}];
+export const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", component: LandingComponent }
+];
