4f33215ffbde27f8c1e55303a8b17fee4a215144
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-04-06 19:21:06 +0100

    Algo de validacion en el login (falta bastante)

--- tournamaker/src/app/app.component.ts
+++ tournamaker/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './templates/navbar/navbar.component';
 import {FooterComponent} from './templates/footer/footer.component';
+import {FormsModule} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'tournamaker';
 }
--- tournamaker/src/app/pages/forms/login/login.component.ts
+++ tournamaker/src/app/pages/forms/login/login.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule, CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
 
-  onFormSubmit() {
+  user = {
+    id: 0,
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    avatar: ""
+  }
+
+  usersFromJSON: any[] = [];
 
 
+  onFormSubmit(userForm: NgForm) {
+
+    console.log(userForm.controls['email'].value);
   }
 }
 
