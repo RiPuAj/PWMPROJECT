47f875a690fff2d2ba7352e0a5c02ed2efa92975
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-04-07 18:54:46 +0100

    Validaciones required de register

--- tournamaker/src/app/pages/forms/register-form/register-form.component.ts
+++ tournamaker/src/app/pages/forms/register-form/register-form.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {User} from '../../../models/user.model';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
-  registerFormSubmit(){
+  registerUser: User = {
+    id: 0,
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    avatar: ""
+  }
+
+  formUser = {
+    id: 0,
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+    avatar: ""
+  }
+
+  onFormSubmit(userForm: NgForm){
 
   }
 }
