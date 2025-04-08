0556e08fca9f5a9ff8ae356c6835f152a1726627
Jose Manuel <jose.diaz168@alu.ulpgc.es>
2025-04-08 18:06:36 +0100

    routing allmatches, alltournaments, creatematch, createtournament

--- tournamaker/src/app/app.routes.ts
+++ tournamaker/src/app/app.routes.ts
@@ -2,9 +2,25 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/forms/login/login.component';
 import {LandingComponent} from './pages/landing/landing.component';
 import {RegisterFormComponent} from './pages/forms/register-form/register-form.component';
+import {AllMatchesComponent} from './pages/all-matches/all-matches.component';
+import {AllTournamentsComponent} from './pages/all-tournaments/all-tournaments.component';
+import {CreateMatchComponent} from './pages/createForms/create-match/create-match.component';
+import {CreateTeamComponent} from './pages/createForms/create-team/create-team.component';
+import {CreateTournamentPageComponent} from './pages/createForms/create-tournament-page/create-tournament-page.component';
+import {MatchInfoPageComponent} from './pages/match-info-page/match-info-page.component';
+import {TournamentViewComponent} from './pages/tournament-view/tournament-view.component';
+import {UserPageComponent} from './pages/user-page/user-page.component';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "", component: LandingComponent },
-  { path: "register", component: RegisterFormComponent }
+  { path: "register", component: RegisterFormComponent },
+  { path: "all-matches", component: AllMatchesComponent },
+  { path: "all-tournaments", component: AllTournamentsComponent },
+  { path: "create-match", component: CreateMatchComponent },
+  { path: "create-team", component: CreateTeamComponent },
+  { path: "create-tournament", component: CreateTournamentPageComponent },
+  { path: "match-info", component: MatchInfoPageComponent },
+  { path: "tournament-view", component: TournamentViewComponent },
+  { path: "user-page", component: UserPageComponent }
 ];
--- tournamaker/src/app/templates/navbar/navbar.component.ts
+++ tournamaker/src/app/templates/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import {Component, NgModule} from '@angular/core';
 import {CommonModule, NgIf} from '@angular/common';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
